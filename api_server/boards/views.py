978938a0094c61696b0655eadfe623f4a7fc99e2
AhnDongwon <lemontech119@gmail.com>
2020-07-25 22:55:32 +0900

    DRF로 api 서버 개발(3) - 댓글작성

--- api_server/boards/views.py
+++ api_server/boards/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 
-from .serializers import BoardSerializer
-from .models import Board
+from .serializers import BoardSerializer, CommentSerializer
+from .models import Board, Comment
 
 
 class BoardViewSet(viewsets.ModelViewSet):
@@ -12,3 +12,12 @@ class BoardViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)

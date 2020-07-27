13318d1ef01f69db3ab43229b0e08dba993379b8
AhnDongwon <lemontech119@gmail.com>
2020-07-28 00:00:43 +0900

    DRF로 api 서버 개발(5) - email

---
+++ api_server/testapi/views.py
@@ -0,0 +1,18 @@
+from django.core.mail.message import EmailMessage
+from api_server import settings
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
+FROM_EMAIL = settings.EMAIL_HOST_USER
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def send_test_email(requset):
+    subject = "이메일 테스트2"
+    to = ['lemontech119@gmail.com']
+    message = "email test가 성공했습니다."
+    EmailMessage(subject=subject, body=message, to=to, from_email=FROM_EMAIL).send()
+
+    return Response({"message": "ok"})
